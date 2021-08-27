0ea5a6d8647a774f56498e1cd54b426acbe17a2a
Muammarbanana <muammarfanani2@gmail.com>
2021-08-27 16:51:25 +0700

    add customappbar test1 page

--- lib/view/test1/test1_page.dart
+++ lib/view/test1/test1_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ordo_test/custom_widgets/custom_appbar.dart';
 
 class Test1Page extends StatefulWidget {
   const Test1Page({Key? key}) : super(key: key);
@@ -10,6 +11,29 @@ class Test1Page extends StatefulWidget {
 class _Test1PageState extends State<Test1Page> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: const Color(0xFFFCF8F8),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(18, screenHeight * 0.03, 18.43, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CustomAppBar(
+                backButtonAsset: "assets/rounded_arrow_green.jpg",
+                title: "LACAK PESANAN",
+                titleColor: 0xFF545454,
+                iconAssets: [
+                  "assets/shopping_bag.png",
+                ],
+              ),
+              SizedBox(height: screenHeight * 0.03),
+              Container(), //peta
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
