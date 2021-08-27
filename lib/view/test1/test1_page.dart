95dc64dc0b50184c6fd757241c1ededb31c4beb6
Muammarbanana <muammarfanani2@gmail.com>
2021-08-27 20:28:59 +0700

    test1 page modal done

--- lib/view/test1/test1_page.dart
+++ lib/view/test1/test1_page.dart
@@ -36,8 +36,7 @@ class _Test1PageState extends State<Test1Page> {
               ),
             ),
             DraggableScrollableSheet(
-              initialChildSize: 0.28,
-              minChildSize: 0.28,
+              initialChildSize: 0.25,
               builder: (context, scrollController) {
                 return SingleChildScrollView(
                   controller: scrollController,
@@ -166,8 +165,222 @@ class _Test1PageState extends State<Test1Page> {
             ],
           ),
           const SizedBox(height: 31),
+          Padding(
+            padding: const EdgeInsets.only(left: 34),
+            child: Row(
+              children: [
+                SizedBox(
+                  height: 17,
+                  width: 17,
+                  child: Image.asset("assets/clock.png"),
+                ),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Status",
+                      style: TextStyle(
+                        fontFamily: 'OpenSans',
+                        fontSize: 14,
+                        color: Color(0xFF47623F),
+                      ),
+                    ),
+                    Text(
+                      "Sedang Mengambil Barang",
+                      style: TextStyle(
+                        fontFamily: 'OpenSans',
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF061737),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 40),
+            child: SizedBox(
+              width: 5,
+              child: Image.asset("assets/dots.png"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 34),
+            child: Row(
+              children: [
+                SizedBox(
+                  height: 17,
+                  width: 17,
+                  child: Image.asset("assets/pin.png"),
+                ),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Alamat Anda",
+                      style: TextStyle(
+                        fontFamily: 'OpenSans',
+                        fontSize: 14,
+                        color: Color(0xFF47623F),
+                      ),
+                    ),
+                    Text(
+                      "Taman Indah Dago No. 612",
+                      style: TextStyle(
+                        fontFamily: 'OpenSans',
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF061737),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 19),
+          const Text(
+            "Pesanan",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(height: 19),
+          _buildOrder(
+            "assets/stroberi.png",
+            "Strawberry",
+            "100 Gram",
+            "Rp. 75.000",
+            "2 Item",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ullamco laboris nisi ut aliquip ex ea commodo .",
+          ),
+          _buildOrder(
+            "assets/stroberi.png",
+            "Nanas",
+            "100 Gram",
+            "Rp. 75.000",
+            "2 Item",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ullamco laboris nisi ut aliquip ex ea commodo .",
+          ),
+          const Text(
+            "Catatan Pesanan",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(height: 3),
+          const Text(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ullamco laboris nisi ut aliquip ex ea commodo .",
+            style: TextStyle(
+              fontSize: 11,
+            ),
+            textAlign: TextAlign.justify,
+          ),
         ],
       ),
+    );
+  }
+
+  Column _buildOrder(String photo, String name, String weight, String price,
+      String itemCount, String note) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(21),
+                  height: 77,
+                  width: 77,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(150),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.15),
+                        blurRadius: 20.0,
+                      ),
+                    ],
+                  ),
+                  child: Image.asset(photo),
+                ),
+                const SizedBox(width: 13),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: const TextStyle(
+                        fontFamily: 'Inter',
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      weight,
+                      style: const TextStyle(
+                        fontFamily: 'Inter',
+                        fontSize: 11,
+                        color: Color(0xFF979696),
+                      ),
+                    ),
+                    Text(
+                      price,
+                      style: const TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF47623F),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 53),
+              child: Text(
+                itemCount,
+                style: const TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        const Text(
+          "Catatan Item",
+          style: TextStyle(
+            fontSize: 11,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(height: 3),
+        Text(
+          note,
+          style: const TextStyle(
+            fontSize: 10,
+          ),
+          textAlign: TextAlign.justify,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 14, bottom: 19),
+          width: double.infinity,
+          height: 1,
+          color: Colors.grey.withOpacity(0.3),
+        )
+      ],
     );
   }
 }
